f3254ac1e94eddcfae15acd6888aa503593d4aaf
Neagu Tudor <tudorneagu7@gmail.com>
2022-10-01 17:15:45 +0300

    added the js and card

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-class fetch {
+//fetch the API
+class Fetch {
   async getCurrent(input) {
     const myKey = "a37f6c68ddfa364db8e95a77e1533dda";
     const response = await fetch(
@@ -10,3 +11,42 @@ class fetch {
     return data;
   }
 }
+
+class UI {
+  constructor() {
+    this.uiContainer = document.querySelector("#content");
+    this.city;
+    this.defaultCity = "Berlin";
+  }
+
+  populateUI(data) {
+    this.uiContainer.innerHTML = `
+        <div class="card mx-auto mt-5" style="width: 18rem;">
+        <div class="card-body justify-content-center">
+            <h5 class="card-title">${data.name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Highs of ${data.main.temp_max}. Lows of ${data.main.temp_min}</h6>
+            <p class="card-text ">Weather conditions are described as: ${data.weather[0].description}</p>
+            
+        </div>
+    </div>
+    
+        
+        `;
+  }
+  clearUI() {
+    uiContainer.innerHTML = "";
+  }
+}
+const ft = new Fetch();
+const ui = new UI();
+
+const input = document.querySelector(".searchCity");
+const button = document.querySelector(".submit");
+
+button.addEventListener("click", () => {
+  const inputValue = input.value;
+
+  ft.getCurrent(inputValue).then((data) => {
+    ui.populateUI(data);
+  });
+});
